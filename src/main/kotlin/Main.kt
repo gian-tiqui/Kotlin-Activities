1005d5e97097ca8dc3b8ddb2667fb58ce35603cb
gian <michaelgiantiqui3@gmail.com>
2023-07-18 11:47:25 +0800

    idk

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,4 +10,40 @@ fun main(args: Array<String>) {
     root.inverse(root.root)
 
     root.inOrder(root.root)
+
+    val PI = 3.14F
+    var num = 1
+    var name = "gian"
+
+    println(name[3])
+    println(name.indexOf("ia"))
+
+    val n = 2
+
+    val n2 = when (n % 2) {
+        0 -> "Even"
+        else -> "Odd"
+    }
+
+    println(n2)
+
+    var n3 = name.length - 1
+    var temp = ""
+
+    while (n3 >= 0) {
+        temp+=name[n3]
+        n3--
+    }
+
+    name = temp
+
+    println(name)
+
+    for (char in name) {
+        println(char)
+    }
+
+    for (ite in 1..10) {
+        print(ite)
+    }
 }
