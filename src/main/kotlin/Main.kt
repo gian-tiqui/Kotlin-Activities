7225b390670db8b4daabcd68ab7a9a8dd6c328eb
gian <michaelgiantiqui3@gmail.com>
2023-07-22 15:15:14 +0800

    meow

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,11 @@
 import basics.isAnagram
+import java.util.Scanner
 
 fun main() {
-    println(isAnagram("hooh", "ohio"))
+    val scanner = Scanner(System.`in`)
+
+    val s1 = scanner.next()
+    val s2 = scanner.next()
+
+    println(isAnagram(s1, s2))
 }
